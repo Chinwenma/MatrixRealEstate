bad0223104ce9d970922ed3cad13e6bc353ce0c9
Chinwenm Okorie <okoriechinwe1327@gmail.com>
2025-10-18 04:12:07 +0100

    Fix import paths in page.tsx for consistency with module resolution

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import FooterBanner from "./components/footer/FooterBanner";
-import PropertyCategories from "./components/home/Categories";
-import HowItWorks from "./components/home/HowItWorks";
-import WhyChooseUs from "./components/home/WhyChooseUs";
-import FeaturedProperties from "./components/featured/FeaturedPrioperty";
-import Hero from "./components/home/Hero";
+import FooterBanner from "@/app/components/footer/FooterBanner";
+import PropertyCategories from "@/app/components/home/Categories";
+import HowItWorks from "@/app/components/home/HowItWorks";
+import WhyChooseUs from "@/app/components/home/WhyChooseUs";
+import FeaturedProperties from "@/app/components/featured/FeaturedPrioperty";
+import Hero from "@/app/components/home/Hero";
 
 export default function Home() {
   return (
