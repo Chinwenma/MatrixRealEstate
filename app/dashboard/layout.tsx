31ebc7f59ad2c923b2aa47f47fbcc1070620dbe8
Chinwenm Okorie <okoriechinwe1327@gmail.com>
2026-03-03 14:15:23 +0100

    Refactor code structure for improved readability and maintainability

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Matrix Real Estate",
+  description:
+    " Find your dream home with Matrix Real Estate - Your trusted partner in property buying, selling, and renting.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className=" scroll-smooth">{children}</body>
+    </html>
+  );
+}
